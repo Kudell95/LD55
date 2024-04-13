e9c34c36af73a1cc2905396174bd3ad2027e0196
kudell95 <kudell95@hotmail.com>
2024-04-13 14:44:29 +1000

    ConfigSO Updates

--- Assets/Scripts/ScriptableObjects/ConfigSO.cs
+++ Assets/Scripts/ScriptableObjects/ConfigSO.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Config", menuName = "Config/GameConfig", order = 1)]
 public class ConfigSO : ScriptableObject
 {
-	
+	[Header("Cards")]
+	public float CommonCardWeight;
+	public float RareCardWeight;
+	public float EpicCardWeight;
+	public float LegendaryCardWeight;
 	
 }
