395fc29f934df53dfd7786c902c32c30affe9779
Pawnda <au.jacklang@gmail.com>
2024-04-13 14:45:11 +1000

    OpponentSO Update

---
+++ Assets/Scripts/ScriptableObjects/Opponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opponent : IOpponent
+{
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+}
