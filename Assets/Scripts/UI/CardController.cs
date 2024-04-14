883f02ecf6a6531890da1a407d6044841d059e77
Pawnda <au.jacklang@gmail.com>
2024-04-14 13:12:01 +1000

    Undo Dissolve

--- Assets/Scripts/UI/CardController.cs
+++ Assets/Scripts/UI/CardController.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,23 +18,10 @@ public class CardController : MonoBehaviour
 	public TextMeshProUGUI DescriptionText;
 	public TextMeshProUGUI ManaText;
 	public Image CardImage;
-
-	//for animation
-	public CanvasGroup TextCanvasGroup;
-	public Image CardFrameImage;
-    public Image ItemImage;
-    private Vector3 _startPos;
-	private Vector3 _startScale;
-	public RectTransform ImageRectTransform;
-	
 	
 	public void Start()
 	{
 		GetRandomCard();
-
-		//for animation
-		_startPos = ImageRectTransform.anchoredPosition;
-		_startScale = transform.localScale;
     }
 	
 	public void SetCard(Card card)
@@ -65,39 +53,14 @@ public class CardController : MonoBehaviour
 		}
 		#endif
 	}
-
-	public IEnumerator AnimateUseCard(bool startingAnimation)
-	{
-		//fade text
-		TextCanvasGroup.DOFade(0f, 1f);
-
-		float moveTime = 1f;
-		float elapsedTime = 0f;
-		float dissolveAmount = 0f;
-
-		while (elapsedTime < moveTime)
-		{
-			elapsedTime += Time.deltaTime;
-			dissolveAmount += Time.deltaTime;
-
-			CardFrameImage.material.SetFloat("_DissolveAmount", dissolveAmount);
-			ImageRectTransform.DOLocalMoveY(ImageRectTransform.position.y + 0.7f, 2.2f);
-
-			yield return null;
-		}
-
-		ItemImage.DOFade(0f, 2f);
-    }
 	
 	
 	public void UseCard()
 	{
-		StartCoroutine(AnimateUseCard(true));
 		CurrentCard.PlayCard();
 	}
 
     public void OnDestroy()
     {
-		StartCoroutine(AnimateUseCard(false));
     }
 }
